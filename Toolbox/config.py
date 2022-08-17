c9fba99868e4c7f217e0592ff9fadc67f24e7e84
KienTT2 <kientt2@rikkeisoft.com>
2022-08-17 15:18:42 +0700

    update

--- Toolbox/config.py
+++ Toolbox/config.py
@@ -1,14 +1,20 @@
-PATH_TO_LIST_TRUYEN = '../truyenfull_list.json'
-PATH_TO_TRUYEN_DES = '../truyen_description.json'
-PATH_TO_CHAPTER_FOLDER = '../truyenfull'
-PATH_TO_CATEGORIES_LIST = '../category_id_list.json'
+PATH_TO_LIST_TRUYEN = 'truyenfull_list.json'
+PATH_TO_TRUYEN_DES = 'truyen_description.json'
+PATH_TO_CHAPTER_FOLDER = 'truyenfull'
+PATH_TO_TAXONOMY_LIST = '{}_id_list.json'
 
-HOST_PATH = "http://192.168.1.60/truyenraw-wordpress"
+
+AUTHOR = "tac-gia"
+SOURCE = "nguon"
+CATEGORY = "categories"
+
+HOST_PATH = "http://localhost:8081/truyenraw"
+
+# HOST_PATH = "http://truyenraw.net"
 
 AUTH = ('admin', 'Thuong@1212')
 
 INSERT_POST_API = "{}/wp-json/wp/v2/posts".format(HOST_PATH)
 INSERT_CHAP_API = "{}/wp-json/wp/v2/chap".format(HOST_PATH)
-INSERT_CATEGORY_API = "{}/wp-json/wp/v2/categories".format(HOST_PATH)
-INSERT_AUTHOR_API = "{}/wp-json/wp/v2/tac-gia".format(HOST_PATH)
-INSERT_SOURCE_API = "{}/wp-json/wp/v2/nguon".format(HOST_PATH)
+TAXONOMY_API = "{}/wp-json/wp/v2/{}"
+UPLOAD_MEDIA_API = "{}/wp-json/wp/v2/media".format(HOST_PATH)
