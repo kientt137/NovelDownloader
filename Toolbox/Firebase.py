bdb68e2889a8354dc307ff30f122a80df04f4039
Tran Trung Kien <kientt13.7@gmail.com>
2024-07-11 15:56:03 +0000

    remove key

--- Toolbox/Firebase.py
+++ Toolbox/Firebase.py
@@ -1,9 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate("truyenraw-64e91-firebase-adminsdk-9chfa-b94f0c803b.json")
+cred = credentials.Certificate("")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://truyenraw-64e91-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    'databaseURL': ''
 })
 ref = db.reference('/')
 print(ref.child('1').get())
